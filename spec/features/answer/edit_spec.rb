71c295ec9a8ed20ba3b8cd21cdc99048e67d0da5
Anakin Skywalker <mutovin9@mail.ru>
2020-10-26 02:51:53 -0400

    "Active Storage.Автор ответа может добавить файлы при редактировании своего ответа"

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -54,6 +54,22 @@ feature 'User can edit his answer', %q{
     scenario "tries to edit other user's question" do
       expect(page).to_not have_css("a[data-answer-id='#{other_answer.id}']")
     end
+
+    scenario 'edits his answer with attached files', js:true do
+      click_on 'Edit'
+
+      within '.answers' do
+        fill_in 'Your answer', with: 'edited answer'
+        attach_file 'File', %W[#{Rails.root}/spec/rails_helper.rb #{Rails.root}/spec/spec_helper.rb]
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+      within '.answers' do
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
 end
