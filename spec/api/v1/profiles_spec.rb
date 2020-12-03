e3f981761715ae4802ea39248c58c4bdb80005f6
Anakin Skywalker <mutovin9@mail.ru>
2020-12-03 11:27:10 -0500

    'REST API.Профиля пользователя (/api/v1/profiles/me)           Списка профилей всех пользователей, за исключением аутентифицированного           Списка вопросов'

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -37,14 +37,51 @@ describe 'Profiles API', type: :request do
 
       it 'returns all public fields' do
         %w[id email created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
       it 'dose not return private fields' do
         %w[password encrypted_password].each do |attr|
-          expect(json).not_to have_key(attr)
+          expect(json['user']).not_to have_key(attr)
         end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles/' do
+    let(:me) { create(:user) }
+    let!(:users) { create_list(:user, 4) }
+    let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+    let(:user_response) { json['users'].first }
+    let(:user) { users.first }
+    let(:api_path) { '/api/v1/profiles/' }
+
+    before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    it 'returns 200 status' do
+      expect(response).to be_successful
+    end
+
+    it 'returns all public fields' do
+      %w[id email created_at updated_at].each do |attr|
+        expect(user_response[attr]).to eq user.send(attr).as_json
+      end
+    end
+
+    it 'does not return private fields' do
+      %w[password encrypted_password].each do |attr|
+        expect(user_response).not_to have_key(attr)
+      end
+    end
+
+    it 'users list not contains current user' do
+      json.each do |user|
+        expect(user).not_to include('id' => me.id)
       end
     end
   end
