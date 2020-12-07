17074edc6d7fdb3da88657717d5c773ed39dff9a
Anakin Skywalker <mutovin9@mail.ru>
2020-12-07 12:37:56 -0500

    'Фоновые задачи.Автор вопроса получает уведомление на email при появлении нового ответа на свой вопрос'

---
+++ spec/jobs/new_answers_notification_job_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe NewAnswersNotificationJob, type: :job do
+  let(:service) { double('NewAnswersNotification') }
+  let(:user) { create(:user) }
+  let(:question) { create(:question, author: user) }
+  let(:answer) { create(:answer, question: question, author: user) }
+
+  it 'calls NewAnswersNotification#send_notification(answer)' do
+    allow(NewAnswersNotification).to receive(:new).and_return(service)
+    expect(service).to receive(:send_notification).with(question.author, answer)
+    NewAnswersNotificationJob.perform_now(question.author, answer)
+  end
+end
---
+++ spec/mailers/new_answers_notification_mailer_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe NewAnswersNotificationMailer, type: :mailer do
+  let(:user) { create(:user) }
+  let(:question) { create(:question, author: user) }
+  let!(:answer) { create(:answer, question: question, author: user) }
+  let(:mail_for_author) { NewAnswersNotificationMailer.send_notification(question.author, answer) }
+
+  describe 'NewAnswerNotificationMailer#send_notification' do
+    it 'renders the headers' do
+      expect(mail_for_author.subject).to eq("New answer")
+      expect(mail_for_author.to).to eq([question.author.email])
+      expect(mail_for_author.from).to eq(['from@example.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail_for_author.body.encoded).to match(answer.body)
+    end
+  end
+end
+
